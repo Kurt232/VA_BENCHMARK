c310768d3ca84c9b746890ade2b85d3d3d140b42
wjdu <duwenjie24@gmail.com>
2025-05-07 17:41:08 +0800

    0507 defination update

--- bench.py
+++ bench.py
@@ -1,9 +1,11 @@
 import os
 import json
-root_path="../../data1/levels/"
+from definitions import *
+root_path="../../data2/data/"
 excluded_tasks=[]
 model_name="X-Instruct-Blip-7B"
-save_path="../wjdu_AVL"
+save_path="../wjdu0507/"
+
 LLM_MODEL_PATH = "./vicuna"
 
 import torch
@@ -108,146 +110,6 @@ def predict(
 
 
 ### dataset
-
-# 本文件用于保存所有的定义
-maic_cls_list = ['bus', 'hair-dryer', 'pipa', 'man', 'ambulance', 'razor', 'harp', 'tabla', 'bass', 'handpan', 
-        'girl', 'sitar', 'car', 'lion', 'guitar', 'vacuum-cleaner', 'cat', 'mower', 'helicopter', 'boy', 'drum', 
-        'keyboard', 'tuba', 'saw', 'flute', 'cello', 'woman', 'gun', 'accordion', 'violin', 'clarinet', 'erhu', 
-        'saxophone', 'guzheng', 'dog', 'baby', 'horse', 'male', 'wolf', 'bird', 'ukulele', 'piano', 'female', 
-        'marimba', 'not sure', 'no available option']
-
-mvic_cls_list = ['sushi', 'banana', 'cake', 'butterfly', 'bird', 'microphone', 'hamburger', 'pineapple', 
-        'man', 'book', 'sunglasses', 'goat', 'tie', 'cabinetry', 'motorcycle', 'drawer', 'strawberry', 
-        'sheep', 'pasta', 'parrot', 'bull', 'table', 'penguin', 'watch', 'pillow', 'shellfish', 'kangaroo', 
-        'flower', 'paddle', 'rocket', 'helicopter', 'bus', 'mushroom', 'bee', 'tree', 'boat', 'saxophone', 
-        'football', 'lizard', 'violin', 'dog', 'cucumber', 'cello', 'airplane', 'horse', 'drum', 'box', 
-        'rabbit', 'car', 'door', 'orange', 'shelf', 'camera', 'poster', 'lemon', 'cat', 'fish', 'bread', 
-        'piano', 'apple', 'glasses', 'bicycle', 'truck', 'deer', 'woman', 'wheelchair', 'cheese', 'chair', 
-        'plate', 'tomato', 'bed', 'starfish', 'balloon', 'bottle', 'crab', 'beer', 'frog', 'shrimp', 'tower', 
-        'guitar', 'pig', 'peach', 'train', 'pumpkin', 'elephant', 'jellyfish', 'parachute', 'monkey', 'flag',
-        'not sure', 'no available option']
-
-prompt_avl = """
-        Based on the given image and audio, there may be multiple sounding instances.
-        You can choose instance categories from the given categories list.
-        The naming format for instances is: category_id. Instance IDs start from 1, e.g., male_1, dog_2, dog_3, cat_4. 
-        The bbox format is: [x, y, w, h], where x and y represent the coordinates of the top-left corner, and w and h are the width and height. 
-        The final answer must strictly adhere to the following format: 
-        answer={"guzheng_1": "[269, 198, 83, 16]", "guzheng_2": "[147, 196, 75, 13]", "female_3": "[152, 108, 123, 36]"}
-    """
-
-avl_cls_list = ['dog', 'clarinet', 'banjo', 'cat', 'guzheng', 'tree', 'lion', 'tuba', 
-        'ukulele', 'flute', 'piano', 'person', 'violin', 'airplane', 'bass', 'pipa', 
-        'trumpet', 'accordion', 'saxophone', 'car', 'lawn-mower', 'cello', 'bassoon', 
-        'horse', 'guitar', 'erhu', 'not sure', 'no available option']
-
-prompt_avlg = """
-        Please output the answer in a format that exactly matches the following example:
-        answer={'frame_0': [x0, y0, w0, h0], 'frame_1': None, ..., 'frame_9': [x9, y9, w9, h9]}.
-        Note, for [x, y, w, h], where x and y represent the top-left corner of the bounding box, 
-        and w and h represent the width and height of the bounding box.
-    """
-
-avqa_cls_list = ['ukulele', 'cello', 'clarinet', 'violin', 'bassoon', 'accordion', 'banjo', 'tuba', 'flute', 'electric_bass', 'bagpipe', 
-        'drum', 'congas', 'suona', 'xylophone', 'saxophone', 'guzheng', 'trumpet', 'erhu', 'piano', 'acoustic_guitar', 'pipa', 'not sure', 'no available option']
-
-havib_constants = {
-    'L1_LAQA': {
-        'options_sound_clarity': ['first', 'last', 'same', 'not sure'],
-        'options_sound_order': ['sound', 'noise', 'not sure'],
-        'options_sound_volume': ['first', 'last', 'same', 'not sure'],
-        'options_yes_no': ['yes', 'no', 'not sure'],
-    },
-
-    'L1_LIQA': {
-        'get_from_background_binary': ['yes', 'no', 'not sure'],
-        'get_from_image_binary': ['yes', 'no', 'not sure'],
-        'get_from_foreground_binary': ['yes', 'no', 'not sure'],
-        'get_from_image_triple': ['blurred', 'normal', 'clear', 'not sure'],
-        'get_from_3d-task1': ['center', 'left', 'right', 'not sure'],
-        'get_from_3d-task2': ['cone', 'cube', 'cylinder', 'cuboid', 'no available option', 'not sure'],
-        # 'get_from_3d-task3': [0, 1, 2, 3, 4, 5, 6],
-        'get_from_space_hard': ['center', 'top left', 'top center', 'top right', 'bottom left', 'bottom center', 'bottom right', 'no available option', 'not sure'],
-        'get_from_color': ['blue', 'green', 'red', 'puprle', 'yellow', 'no available option', 'not sure'],
-        'get_yes_no': ['yes', 'no', 'not sure'],
-        # 'get_lines_count': [0, 1, 2, 3, 4],
-        'get_lines_direction': ['horizontal', 'vertical', 'inclined', 'not sure'],
-        'get_from_space_easy_area': ['the right one', 'the left one', 'the middle one', 'the bottom one', 'the top one'],
-        'get_from_space_easy_bbrightness': ['the right one', 'the left one', 'the middle one', 'the bottom one', 'the top one'],
-        'options_yes_no': ['yes', 'no', 'not sure'],
-    },
-
-    'L1_LVQA': {
-        'which_object': ['square', 'circle', 'triangle', 'not sure', 'no available option', 'not sure'],
-        'what_shape': ['Triangular pyramid', 'Cone', 'Cube', 'Sphere', 'None', 'not sure'],
-        # 'how_many': [0, 1, 2, 3, 4, 5, 6, 7, 8],
-        'what_movement_2d': ['horizontal', 'inclined', 'vertical', 'no movenment', 'None', 'not sure'],
-        'what_movement_3d': ['Rotation', 'Shrinking', 'Translation', 'Enlarging', 'None', 'not sure'],
-        'what_surface': ['Rough', 'Moderate', 'Smooth', 'None', 'not sure'],
-        'spacial_change': ['Bottom-left to top-right', 'Bottom-right to top-left', 'Top-left to bottom-right', 'Top-right to bottom-left', 'None', 'not sure', 'No movement',],
-        'options_yes_no': ['yes', 'no', 'not sure'],
-    },
-
-    'L2_MAIC': {
-        'maic_cls_list': maic_cls_list,
-        'prompt_maic': "There may be one or more sound-emitting objects in the provided audio. \nPlease strictly output the answer in the format answer={'cls_1': count_1, 'cls_2': count_2}, \nfor example, answer={'dog': 2, 'cat': 3, 'male': 1}. \n"
-    },
-
-    'L2_MVIC': {
-        'mvic_cls_list': mvic_cls_list,
-        'prompt_mvic': "There may be one or more visible objects in the provided image. \nPlease strictly output the answer in the format answer={'cls_1': count_1, 'cls_2': count_2}, \nfor example, answer={'dog': 2, 'cat': 3, 'male': 1}. \n Possible categoris are in the list: mvic_cls_list"
-    },
-
-    'L3_AVH': {
-        'prompt_avh': "Please answer the question based on the given audio and video.",
-        'avh_options_list': ['yes', 'no', 'not sure'],
-    },
-
-    'L3_VAH': {
-        'prompt_vah': "Please answer the question based on the given audio and video.",
-        'vah_options_list': ['yes', 'no', 'not sure'],
-    },
-
-    'L3_AVL': {
-        'prompt_avl': prompt_avl,
-        'avl_cls_list': avl_cls_list,
-    },
-
-    'L3_AVM': {
-        'prompt_avm': 'Please answer the question based on the given audio and video.',
-        'avm_options_list': ['yes', 'no', 'not sure'],
-    },
-
-    'L3_AVR': {
-        'prompt_avr': "Please output the indices of the images list, starting from 0. For example: [], or [0, 3], or [1, 4, 9]."
-    },
-
-    'L3_VAR': {
-        'prompt_var': "Please output the indices of the wavs list, starting from 0. For example: [], or [0, 3], or [1, 4, 9]."
-    },
-
-    'L4_AVC': {
-
-    },
-
-    'L4_AVLG': {
-        'prompt_avlg': prompt_avlg,
-    },
-
-    'L4_AVQA': {
-        'avqa_options_list_is': ['yes', 'no', 'not sure'],
-    },
-
-    'L5_AVLG': {
-        'prompt_avlg': prompt_avlg,
-    },
-
-    'L5_AVQA': {
-        'avqa_cls_list': avqa_cls_list,
-        'avqa_options_list_is': ['yes', 'no', 'not sure'],
-    },
-}
-
 def get_real_options_or_classes(d: dict):
     """ replace the pseudo-options with real options. """
 
